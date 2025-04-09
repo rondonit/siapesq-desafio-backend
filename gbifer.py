cee9022b2ee428081b78f6d9d8aa34e2edb934ac
rondonit <gabriel.cruz@ecomp.ufsm.br>
2025-04-09 14:52:51 -0300

    Comments and main() function

--- gbifer.py
+++ gbifer.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+# Author: Gabriel Cruz
+# Date: 2025-04-09
+# Description: This script uses the GBIF API to search for occurrences of a given species
+# within a specified bounding box and date range.
+# The results are saved to a CSV file.
 
 # TO DO:
-#   - Create main() function
-#   - Rewrite comments in english
-#   - Create functions for better code readability 
+#   - Create functions for each step for better readability
 
 import sys
 from pygbif import occurrences
@@ -11,13 +13,13 @@ import pandas as pd
 import argparse
 
 def parser_arguments():
-    # captura os argumentos de linha de comando
+    # Get the arguments from the command line
     #   --specie
-    #   --bbox (bound box com 4 valores float)
+    #   --bbox (lat, long, 4 float values)
     #   --limit
     #   --begin_date
     #   --end_date
-    #   --out_csv (arquivo de saída dos dados)
+    #   --out_csv (output value)
 
     parser = argparse.ArgumentParser()
 
@@ -30,38 +32,41 @@ def parser_arguments():
 
     return parser.parse_args()
 
-#########################
-# separar em função main?
-args = parser_arguments()
+def main():
+    args = parser_arguments()
 
-# separa os argumentos --bbox para lat_max, lat_min, lon_max, lon_min
-# constroi as strings para o pygbif
-lat_max, lat_min, lon_max, lon_min = args.bbox
-lat = f"{lat_min},{lat_max}"
-lon = f"{lon_min},{lon_max}"
+    # Builds the string for the bounding box
+    #   lat_max, lat_min, lon_max, lon_min
+    lat_max, lat_min, lon_max, lon_min = args.bbox
+    lat = f"{lat_min},{lat_max}"
+    lon = f"{lon_min},{lon_max}"
 
-# usa --begin_date e --end_date para montar string
-# no formato yyyy-MM-dd,yyyy-MM-dd
-eventDate_str = f"{args.begin_date},{args.end_date}"
+    # Builds the string for the date range
+    #  format: YYYY-MM-DD,YYYY-MM-DD
+    eventDate_str = f"{args.begin_date},{args.end_date}"
 
-# faz a busca na API do GBIF usando a biblioteca pygbif
-# https://pygbif.readthedocs.io/en/latest/modules/occurrence.html
-gbif_data = occurrences.search(
-    scientificName = args.specie,
-    decimalLatitude = lat,
-    decimalLongitude = lon,
-    eventDate = eventDate_str,
-    limit = args.limit,
-    hasCoordinate = True
-)
+    # Search for occurrences using the GBIF API
+    # https://pygbif.readthedocs.io/en/latest/modules/occurrence.html
+    gbif_data = occurrences.search(
+        scientificName = args.specie,
+        decimalLatitude = lat,
+        decimalLongitude = lon,
+        eventDate = eventDate_str,
+        limit = args.limit,
+        hasCoordinate = True
+    )
 
-# cria DataFrame com os dados recebidos
-data_df = pd.DataFrame(gbif_data["results"])
+    # Create dataframe from the results
+    data_df = pd.DataFrame(gbif_data["results"])
 
-# filtra as colunas
-gbif_cols = data_df.columns.tolist()
-final_cols = ["decimalLongitude", "decimalLatitude", "year", "day", "month"]
-data_df_selected = data_df[[c for c in final_cols if c in gbif_cols]]
+    # Select the columns of interest
+    #   decimalLongitude, decimalLatitude, year, day, month
+    gbif_cols = data_df.columns.tolist()
+    final_cols = ["decimalLongitude", "decimalLatitude", "year", "day", "month"]
+    data_df_selected = data_df[[c for c in final_cols if c in gbif_cols]]
 
-# salva em .csv
-data_df_selected.to_csv(args.out_csv, index=False)
+    # Save the dataframe to a CSV file
+    data_df_selected.to_csv(args.out_csv, index=False)
+
+if __name__ == "__main__":
+    main()
